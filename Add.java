0e7a76e658dec92a7ab6f719c7439b360d41bbb5
Viveksingh99 <vivek22jan99@gmail.com>
2024-02-27 23:14:13 +0530

    learn if else and switch

--- Add.java
+++ Add.java
@@ -1,9 +1,20 @@
 public class Add {
     public static void main(String a[])
 {
+
+    // datatypes and variable 
+
+    // float num = 4.5f;
+    // double num3 = 5.5;
+    // byte by = 127;
+    // short sh = 558;
+    // long l = 3723l;
+    // char name = 'vivek';
+    // boolean b= true;
     int nun1 = 2;
     int num2 = 3;
     int result = num2 + nun1;
     System.out.println(result);
 }
 }
+ 
---
+++ conditional/Ternary.java
@@ -0,0 +1,10 @@
+package conditional;
+
+public class Ternary {
+    public static void main(String a[]) {
+        int n = 3;
+        int result = 0;
+        result = n % 2 == 0 ? 4 : 5;
+        System.out.println(result);
+    }
+}
