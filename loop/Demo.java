d72365fc9a2404c949b66638286125951468d512
Viveksingh99 <vivek22jan99@gmail.com>
2024-02-29 00:56:54 +0530

    learn object and class

---
+++ loop/Demo.java
@@ -0,0 +1,24 @@
+package loop;
+
+public class Demo {
+    public static void main(String a[]) {
+        // int i = 0;
+        // while (i <= 5) {
+        //     System.out.println("vivek" + i);
+        //     int j = 0;
+        //     while (j <= 3) {
+        //         System.out.println("singh" + j);
+        //         j++;
+        //     }
+        //     i++;
+        // }
+
+        // do {
+        //     System.out.println("vivek" + i);
+        //     i++;
+        // } while (i<=5);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("vivek");
+        }
+    }
+}
